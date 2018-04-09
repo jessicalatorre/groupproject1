2853be5b49b04c4876cbe08e8f58f894ab2aa85d
Jeff Swafford <jlswafford789@yahoo.com>
2018-04-09 18:16:55 -0500

    gots some api calls

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -33,104 +33,104 @@ console.log(artistsReturned);
 
 
 
-
+var yTresults = [];
 var artistQuery = "peral jam";
 
-/** JEFF: GOOGLES MAGIC MAKE DO WORK GOOD CODE FOR YOUTUBE
-	JEFF: GOD HELP US ALL**/
+ /** JEFF: GOOGLE'S MAGIC MAKE DO WORK GOOD CODE FOR YOUTUBE
+ 	JEFF: GOD HELP US ALL**/
 
- /***** START BOILERPLATE CODE: Load client library, authorize user. *****/
+  /***** START BOILERPLATE CODE: Load client library, authorize user. *****/
 
-  // Global variables for GoogleAuth object, auth status.
-  var GoogleAuth;
+   // Global variables for GoogleAuth object, auth status.
+   var GoogleAuth;
 
-  /**
-   * Load the API's client and auth2 modules.
-   * Call the initClient function after the modules load.
-   */
-  function handleClientLoad() {
-    gapi.load('client:auth2', initClient);
-  }
+   /**
+    * Load the API's client and auth2 modules.
+    * Call the initClient function after the modules load.
+    */
+   function handleClientLoad() {
+     gapi.load('client:auth2', initClient);
+   }
 
-  function initClient() {
-  	console.log("in client init");
-    // Initialize the gapi.client object, which app uses to make API requests.
-    // Get API key and client ID from API Console.
-    // 'scope' field specifies space-delimited list of access scopes
+   function initClient() {
+   	console.log("in client init");
+     // Initialize the gapi.client object, which app uses to make API requests.
+     // Get API key and client ID from API Console.
+     // 'scope' field specifies space-delimited list of access scopes
 
-    gapi.client.init({
-        'apiKey': youtubeAPIKey,
-        'clientId': 'omega-remix',
-        'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'],
-         'scope': 'https://www.googleapis.com/auth/youtube.force-ssl https://www.googleapis.com/auth/youtubepartner'
-    }).then(function () {
-    console.log("in client init then");
-      GoogleAuth = gapi.auth2.getAuthInstance();
+     gapi.client.init({
+         'apiKey': youtubeAPIKey,
+         'clientId': '130884740564-tkjb24s0p7bhrptrjbins2nlc4cp8h7m.apps.googleusercontent.com',
+         'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'],
+          'scope': 'https://www.googleapis.com/auth/youtube.force-ssl https://www.googleapis.com/auth/youtubepartner'
+     }).then(function () {
+     console.log("in client init then");
+       GoogleAuth = gapi.auth2.getAuthInstance();
 
-      // Listen for sign-in state changes.
-      GoogleAuth.isSignedIn.listen(updateSigninStatus);
+       // Listen for sign-in state changes.
+       GoogleAuth.isSignedIn.listen(updateSigninStatus);
 
-      // Handle initial sign-in state. (Determine if user is already signed in.)
-      setSigninStatus();
+       // Handle initial sign-in state. (Determine if user is already signed in.)
+       setSigninStatus();
 
-      // Call handleAuthClick function when user clicks on "Authorize" button.
-      $('#execute-request-button').click(function() {
-      	console.log("auth clicked");
-        handleAuthClick(event);
-      }); 
-    });
-  }
+       // Call handleAuthClick function when user clicks on "Authorize" button.
+       $('#execute-request-button').click(function() {
+       	console.log("auth clicked");
+         handleAuthClick(event);
+       }); 
+     });
+   }
 
-  function handleAuthClick(event) {
-    // Sign user in after click on auth button.
-    GoogleAuth.signIn();
-  }
+   function handleAuthClick(event) {
+     // Sign user in after click on auth button.
+     GoogleAuth.signIn();
+   }
 
-  function setSigninStatus() {
-    var user = GoogleAuth.currentUser.get();
-    isAuthorized = user.hasGrantedScopes('https://www.googleapis.com/auth/youtube.force-ssl https://www.googleapis.com/auth/youtubepartner');
-    // Toggle button text and displayed statement based on current auth status.
-    if (isAuthorized) {
-      defineRequest();
-    }
-  }
+   function setSigninStatus() {
+     var user = GoogleAuth.currentUser.get();
+     isAuthorized = user.hasGrantedScopes('https://www.googleapis.com/auth/youtube.force-ssl https://www.googleapis.com/auth/youtubepartner');
+     // Toggle button text and displayed statement based on current auth status.
+     if (isAuthorized) {
+       defineRequest();
+     }
+   }
 
-  function updateSigninStatus(isSignedIn) {
-    setSigninStatus();
-  }
+   function updateSigninStatus(isSignedIn) {
+     setSigninStatus();
+   }
 
-  function createResource(properties) {
-    var resource = {};
-    var normalizedProps = properties;
-    for (var p in properties) {
-      var value = properties[p];
-      if (p && p.substr(-2, 2) == '[]') {
-        var adjustedName = p.replace('[]', '');
-        if (value) {
-          normalizedProps[adjustedName] = value.split(',');
-        }
-        delete normalizedProps[p];
-      }
-    }
-    for (var p in normalizedProps) {
-      // Leave properties that don't have values out of inserted resource.
-      if (normalizedProps.hasOwnProperty(p) && normalizedProps[p]) {
-        var propArray = p.split('.');
-        var ref = resource;
-        for (var pa = 0; pa < propArray.length; pa++) {
-          var key = propArray[pa];
-          if (pa == propArray.length - 1) {
-            ref[key] = normalizedProps[p];
-          } else {
-            ref = ref[key] = ref[key] || {};
-          }
-        }
-      };
-    }
-    return resource;
-  }
+   function createResource(properties) {
+     var resource = {};
+     var normalizedProps = properties;
+     for (var p in properties) {
+       var value = properties[p];
+       if (p && p.substr(-2, 2) == '[]') {
+         var adjustedName = p.replace('[]', '');
+         if (value) {
+           normalizedProps[adjustedName] = value.split(',');
+         }
+         delete normalizedProps[p];
+       }
+     }
+     for (var p in normalizedProps) {
+       // Leave properties that don't have values out of inserted resource.
+       if (normalizedProps.hasOwnProperty(p) && normalizedProps[p]) {
+         var propArray = p.split('.');
+         var ref = resource;
+         for (var pa = 0; pa < propArray.length; pa++) {
+           var key = propArray[pa];
+           if (pa == propArray.length - 1) {
+             ref[key] = normalizedProps[p];
+           } else {
+             ref = ref[key] = ref[key] || {};
+           }
+         }
+       };
+     }
+     return resource;
+   }
 
-  function removeEmptyParams(params) {
+   function removeEmptyParams(params) {
     for (var p in params) {
       if (!params[p] || params[p] == 'undefined') {
         delete params[p];
@@ -142,6 +142,9 @@ var artistQuery = "peral jam";
   function executeRequest(request) {
     request.execute(function(response) {
       console.log(response);
+      //response object gotten here
+      yTresults.push(response);
+
     });
   }
 
@@ -176,12 +179,34 @@ var artistQuery = "peral jam";
 
 buildApiRequest('GET',
                 '/youtube/v3/search',
-                {'maxResults': '25',
+                {'maxResults': '5',
                  'part': 'snippet',
                  'q': artistQuery,
-                 'type': ''});
-
+                 'type': 'video',
+                 'videoCategoryId': '10',
+                 'videoEmbeddable':'true',});
   }
 
   /** JEFF: END OF DO MAKE WORK GOOD GOOGLE CODE
-  	  JEFF: I AM *NOT* Responsable for this code**/
+  	  JEFF: I am *NOT* Responsable for this code**/
+function getVideoIds(artistQuery){
+// buildApiRequest('GET',
+//                 '/youtube/v3/search',
+//                 {'maxResults': '5',
+//                  'part': 'snippet',
+//                  'q': artistQuery,
+//                  'type': 'video',
+//                  'videoCategoryId': '10',
+//                  'videoEmbeddable':'true',});
+setTimeout(function(){buildApiRequest('GET',
+                '/youtube/v3/search',
+                {'maxResults': '5',
+                 'part': 'snippet',
+                 'q': artistQuery,
+                 'type': 'video',
+                 'videoCategoryId': '10',
+                 'videoEmbeddable':'true',}
+	)},2000);
+}
+getVideoIds('pearl jam');
+getVideoIds("black sabbath");
